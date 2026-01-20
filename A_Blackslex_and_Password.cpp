94b362bbbd0b9c82329ce592353eaf9373bd0e2f
MARIEM <mariemcharef42@gmail.com>
2026-01-20 10:04:13 +0100

    add blackslex

---
+++ A_Blackslex_and_Password.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+void solve() {
+    int n, x,k;
+    cin>>k>>x;
+    cout<<k*x+1<<endl;
+
+
+
+
+    
+    
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
